4b3ccc8167e4d4bd9b17524beb31ee7139e82ae4
Unzor <71789103+Unzor@users.noreply.github.com>
2021-10-26 15:35:06 -0500

    Update translator.js

--- translator.js
+++ translator.js
@@ -1,13 +1,11 @@
-function translate_to_asciiscript(code, cb){
 var int = 0;
 var fin=[];
-var str=code;
+var str=process.argv[2];
 var array=str.split('');
 array.forEach(function(entry){
 int = int + 1;
 fin.push(entry.charCodeAt(0) + '-');
 if (int == array.length){
-cb(fin.join('').slice(0, -1));
+console.log(fin.join('').slice(0, -1));
 }
 })
-}
